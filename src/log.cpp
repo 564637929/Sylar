5b55f4924c7edd4997b378cbc6d7996e029792c1
hujiajin <564637929@qq.com>
2025-01-12 20:26:32 +0800

    Modify format

--- src/log.cpp
+++ src/log.cpp
@@ -1,47 +1,47 @@
 #include "log.h"
 
-namespace sylar{
+namespace sylar {
 
 Logger::Logger(const std::string& name = "root")
-:m_name(name){
+:m_name(name) {
 
 }
 
-void Logger::log(LogLevel::Level level,LogEvent::ptr event){
-    if(level >= m_level){
-        for(auto & i: m_appenders){
-            i->log(level,event);
+void Logger::log(LogLevel::Level level, LogEvent::ptr event) {
+    if (level >= m_level) {
+        for (auto & i: m_appenders) {
+            i->log(level, event);
         }
     }
 }
 
-void Logger::debug(LogEvent::ptr event){
-    debug(LogLevel::DEBUG,event);
+void Logger::debug(LogEvent::ptr event) {
+    debug(LogLevel::DEBUG, event);
 }
 
-void Logger::info(LogEvent::ptr event){
-    debug(LogLevel::INFO,event);
+void Logger::info(LogEvent::ptr event) {
+    debug(LogLevel::INFO, event);
 }
 
-void Logger::warn(LogEvent::ptr event){
-    debug(LogLevel::WARN,event);
+void Logger::warn(LogEvent::ptr event) {
+    debug(LogLevel::WARN, event);
 }
 
-void Logger::error(LogEvent::ptr event){
-    debug(LogLevel::ERROR,event);
+void Logger::error(LogEvent::ptr event) {
+    debug(LogLevel::ERROR, event);
 }
 
-void Logger::fatal(LogEvent::ptr event){
-    debug(LogLevel::FATAL,event);
+void Logger::fatal(LogEvent::ptr event) {
+    debug(LogLevel::FATAL, event);
 }
 
-void Logger::addAppender(LogAppender::ptr appender){
+void Logger::addAppender(LogAppender::ptr appender) {
     m_appenders.push_back(appender);
 }
 
 void Logger::delAppender(LogAppender::ptr appender){
-    for(auto it = m_appenders.begin();it!=m_appenders.end();++it){
-        if(*it == appender){
+    for(auto it = m_appenders.begin(); it!=m_appenders.end(); ++it) {
+        if (*it == appender) {
             m_appenders.erase(it);
             break;
         }
@@ -49,29 +49,55 @@ void Logger::delAppender(LogAppender::ptr appender){
 }
 
 FileLogAppender::FileLogAppender(const std::string & filename)
-:m_filename(filename){
+:m_filename (filename) {
 
 }
 
-void StdoutLogAppender::log(LogLevel::Level level,LogEvent::ptr event){
-    if(level >= m_level){
+void StdoutLogAppender::log(LogLevel::Level level, LogEvent::ptr event) {
+    if (level >= m_level) {
         std::cout << m_formatter.format(event);
     }
 }
 
-void FileLogAppender::log(LogLevel::Level level,LogEvent::ptr event){
-    if(level >= m_level){
+void FileLogAppender::log(LogLevel::Level level, LogEvent::ptr event) {
+    if (level >= m_level) {
         m_filestream << m_formatter.format(event);
     }
 }
 
 
-bool FileLogAppender::reopen(){
-    if(m_filestream){//如果文件已被打开，则关闭文件后再重新打开
+bool FileLogAppender::reopen() {
+    //如果文件已被打开，则关闭文件后再重新打开
+    if (m_filestream) {
         m_filestream.close();
     }
     m_filestream.open(m_filename);
     return !!m_filestream;
 }
 
+LogFormatter::LogFormatter(const std::string & pattern)
+:m_pattern (pattern) {
+
+}
+
+std::string LogFormatter::format(LogEvent::ptr event) {
+    std::stringstream ss;
+    for (auto &i : m_items) {
+        i->format(ss,event);
+    }
+    return ss.str();
+}
+
+void LogFormatter::init() {
+    std::vector<std::tuple<std::string, std::string, int>> vec;
+    size_t last_pos = 0;
+    for (size_t i = 0; i < m_pattern.size(); ++i) {
+        if (m_pattern[i] == '%') {
+            if()
+        }
+    }
+}
+
 };
+
+
--- src/log.h
+++ src/log.h
@@ -8,10 +8,10 @@
 #include <sstream>
 #include <fstream>
 
-namespace sylar{
+namespace sylar {
 
 //日志事件
-class LogEvent{
+class LogEvent {
 public:
     typedef std::shared_ptr<LogEvent> ptr;
     LogEvent();
@@ -26,23 +26,32 @@ private:
 };
 
 //日志输出地
-class LogAppender{
+class LogAppender {
 public:
-    typedef std::shared_ptr<LogAppender>ptr;
-    virtual void log(LogLevel::Level level,LogEvent::ptr event) = 0;//纯虚函数
-    virtual ~LogAppender(){};       //日志输出的目的地有很多，所以用虚函数来写;析构函数设为虚函数，便于顺利释放
-    void setFormatter(LogFormatter::ptr val){ m_formatter = val;}//设置日志格式
-    LogFormatter::ptr getFormatter() const { return m_formatter;}//获取日志格式
+    typedef std::shared_ptr<LogAppender> ptr;
+
+    //纯虚函数
+    virtual void log(LogLevel::Level level, LogEvent::ptr event) = 0;
+
+    //日志输出的目的地有很多，所以用虚函数来写;析构函数设为虚函数，便于顺利释放
+    virtual ~LogAppender() {};
+
+    //设置日志格式
+    void setFormatter(LogFormatter::ptr val) { m_formatter = val;}
+
+    //获取日志格式
+    LogFormatter::ptr getFormatter() const { return m_formatter;}
 protected:
-    LogLevel::Level m_level;        //针对特定日志来定义日志级别（子类中要用到 m_level 所以为了继承使用（比如StdoutLogAppender和FileLogAppender），设为protected）
+    //针对特定日志来定义日志级别(子类中要用到 m_level 所以为了继承使用(比如StdoutLogAppender和FileLogAppender)，设为protected)
+    LogLevel::Level m_level;
     LogFormatter::ptr m_formatter;
 };
 
 //日志级别
-class LogLevel{
+class LogLevel {
 public:
-    enum Level{
-        DEBUG = 1,                  //最低级别
+    enum Level {
+        DEBUG = 1,  //最低级别
         INFO = 2,
         WARN = 3,
         ERROR = 4,
@@ -51,50 +60,72 @@ public:
 };
 
 //日志格式
-class LogFormatter{
+class LogFormatter {
 public:
-    typedef std::shared_ptr<LogFormatter>ptr;
+    typedef std::shared_ptr<LogFormatter> ptr;
+    LogFormatter(const std::string & pattern);
     std::string format(LogEvent::ptr event);
 private:
+    class FormatItem {
+    public:
+        typedef std::shared_ptr<FormatItem> ptr;
+        virtual ~FormatItem() {}
+        virtual void format(std::ostream &os,LogEvent::ptr event) = 0;
+
+    };
+
+    //用于pattern的解析
+    void init();
+private:
+    std::string m_pattern;
+    std::vector<FormatItem::ptr> m_items;
 };
 
 //日志器
-class Logger{
+class Logger {
 public:
     typedef std::shared_ptr<Logger> ptr;
     Logger(const std::string& name = "root");
-    void log(LogLevel::Level level,LogEvent::ptr event);
+    void log(LogLevel::Level level, LogEvent::ptr event);
     void debug(LogEvent::ptr event);
     void info(LogEvent::ptr event);
     void warn(LogEvent::ptr event);
     void error(LogEvent::ptr event);
     void fatal(LogEvent::ptr event);
-    void addAppender(LogAppender::ptr appender);        //添加日志级别
-    void delAppender(LogAppender::ptr appender);        //删除日志级别
-    LogLevel::Level getLevel() const {return m_level;}  //获取日志级别(const成员函数)
-    void setLevel(LogLevel::Level val){m_level = val;}  //设置日志级别
+
+    //添加日志级别
+    void addAppender(LogAppender::ptr appender);
+
+    //删除日志级别
+    void delAppender(LogAppender::ptr appender);
+
+    //获取日志级别(const成员函数)
+    LogLevel::Level getLevel() const { return m_level;}
+
+    //设置日志级别
+    void setLevel(LogLevel::Level val) { m_level = val;}
 private:
-    std::string m_name;                        //日志名称
-    LogLevel::Level m_level;                   //日志器的日志级别，满足了级别的才会被输出
-    std::list<LogAppender::ptr>m_appenders;    //Appender集合
+    std::string m_name;                         //日志名称
+    LogLevel::Level m_level;                    //日志器的日志级别，满足了级别的才会被输出
+    std::list<LogAppender::ptr> m_appenders;    //Appender集合
 };
 
 //输出到控制台的Appender
 class StdoutLogAppender
-:public LogAppender{
+:public LogAppender {
 public:
-    typedef std::shared_ptr<StdoutLogAppender>ptr;
-    void log(LogLevel::Level level,LogEvent::ptr event) override;
+    typedef std::shared_ptr<StdoutLogAppender> ptr;
+    void log(LogLevel::Level level, LogEvent::ptr event) override;
 private:
 };
 
 //输出到文件的Appender
 class FileLogAppender
-:public LogAppender{
+:public LogAppender {
 public:
-    typedef std::shared_ptr<FileLogAppender>ptr;
+    typedef std::shared_ptr<FileLogAppender> ptr;
     FileLogAppender(const std::string & filename);   //文件名
-    void log(LogLevel::Level level,LogEvent::ptr event) override;
+    void log(LogLevel::Level level, LogEvent::ptr event) override;
     bool reopen();//重新打开文件
 private:
     std::string m_filename;
